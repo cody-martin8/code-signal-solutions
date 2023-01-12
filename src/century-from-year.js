dc3b8b4bd2ccf53fd75bf4694a888f5782774e8f
Cody Martin <12martincody@gmail.com>
2023-01-12 08:05:41 +0000

    Code Signal solution for 'centuryFromYear'.

---
+++ src/century-from-year.js
@@ -0,0 +1,14 @@
+function centuryFromYear(year) {
+  const century = year / 100;
+  const remainder = century % 1;
+  if (remainder === 0) {
+    return century;
+  } else {
+    return century - remainder + 1;
+  }
+}
+
+// Example
+
+centuryFromYear(1901);
+// Expected: 20
