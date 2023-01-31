e5c1a1b688d7a0a40eb2d3beb6e5706dcaa977e2
Cody Martin <12martincody@gmail.com>
2023-01-31 08:40:45 +0000

    Code Signal solution for 'isIPv4Address'.

--- src/is-ipv4-address.js
+++ src/is-ipv4-address.js
@@ -1,17 +1,14 @@
+// Create a function that evaluates a string to check if it passes the IPv4
+// naming rules, where there are four numbers separated by '.' and each number
+// is between 0 and 255
+
 function isIPv4Address(inputString) {
   const ipCheck = inputString.split('.');
   if (ipCheck.length !== 4) {
     return false;
   }
   for (let i = 0; i < ipCheck.length; i++) {
-    const number = parseInt(ipCheck[i]);
-    if (ipCheck[i] < 0 || ipCheck[i] > 255) {
-      return false;
-    }
-    if (number === ipCheck[i]) {
-      return false;
-    }
-    if (!Number.isInteger(ipCheck[i])) {
+    if ((ipCheck[i] < 1 && ipCheck[i] !== '0') || ipCheck[i] > 255 || ipCheck[i] === '' || parseFloat(ipCheck[i]).toString() !== ipCheck[i]) {
       return false;
     }
   }
